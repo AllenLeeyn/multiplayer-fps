b0bb73475bbda618bac1ce5d34318b84784bf207
leeyn <leeyn.shun@gmail.com>
2025-12-16 22:58:20 +0200

    Implement simple json config load and saving

---
+++ crates/fps_config/src/lib.rs
@@ -0,0 +1,38 @@
+use serde::{Deserialize, Serialize};
+use std::path::Path;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Config {
+    pub username: String,
+}
+
+impl Config {
+    pub fn load<P: AsRef<Path>>(path: P) -> Self {
+        match std::fs::read_to_string(&path) {
+            Ok(contents) => serde_json::from_str(&contents).unwrap_or_else(|err| {
+                eprintln!("Failed to parse config: {}. Using default.", err);
+                Self::default()
+            }),
+            Err(_) => {
+                println!("Config file not found at {:?}. Using default.", path.as_ref());
+                Self::default()
+            }
+        }
+    }
+
+    pub fn save<P: AsRef<Path>>(&self, path: P) {
+        if let Ok(json) = serde_json::to_string_pretty(self) {
+            if let Err(err) = std::fs::write(path, json) {
+                eprintln!("Failed to save config: {}", err);
+            }
+        }
+    }
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            username: "set username here".to_string(),
+        }
+    }
+}
